2cfb8563523f2aaa13512b3972ab8a3b36bf1772
jvreca <jure.vreca@ijs.si>
2023-06-16 16:27:08 +0200

    reset

--- src/main/scala/BitReversePingPong.scala
+++ src/main/scala/BitReversePingPong.scala
@@ -5,38 +5,35 @@ package fft
 import chisel3._
 import chisel3.util._
 import chisel3.experimental._
-import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 
 import dsptools._
 import dsptools.numbers._
 import dspblocks.CounterWithReset
+
 import scala.math._
 
-case class BitReversePingPongParams[T <: Data](
-  val proto:          DspComplex[T], // data type
-  val pingPongSize:   Int, // ping pong size or fft size
-  val adjustableSize: Boolean, // include register for determining current ping - pong size
-  val bitReverseDir:  Boolean, // determine whether read or write address is bit reversed
-  val singlePortSRAM: Boolean = false) {
+case class BitReversePingPongParams[T <: Data] (
+  val proto           : DspComplex[T], // data type
+  val pingPongSize    : Int,           // ping pong size or fft size
+  val adjustableSize  : Boolean,       // include register for determining current ping - pong size
+  val bitReverseDir   : Boolean        // determine whether read or write address is bit reversed
+) {
   requireIsChiselType(proto)
 }
 
 object BitReversePingPongParams {
-  def fixed(
-    dataWidth:      Int = 16,
-    binPoint:       Int = 14,
-    pingPongSize:   Int = 16,
-    adjustableSize: Boolean = false,
-    bitReverseDir:  Boolean = true,
-    singlePortSRAM: Boolean = false
-  ): BitReversePingPongParams[FixedPoint] = {
+  def fixed(dataWidth        : Int = 16,
+            binPoint         : Int = 14,
+            pingPongSize     : Int = 16,
+            adjustableSize   : Boolean = false,
+            bitReverseDir    : Boolean = true
+           ): BitReversePingPongParams[FixedPoint] = {
     val proto = DspComplex(FixedPoint(dataWidth.W, binPoint.BP))
     BitReversePingPongParams(
       proto = proto,
       pingPongSize = pingPongSize,
       adjustableSize = adjustableSize,
-      bitReverseDir = bitReverseDir,
-      singlePortSRAM = singlePortSRAM
+      bitReverseDir = bitReverseDir
     )
   }
 }
@@ -44,8 +41,8 @@ object BitReversePingPongParams {
 class BitReversePingPongIO[T <: Data: Real](val params: BitReversePingPongParams[T]) extends Bundle {
   val in = Flipped(Decoupled(params.proto))
   val out = Decoupled(params.proto)
-  val size = if (params.adjustableSize) Some(Input(UInt(log2Up(params.pingPongSize + 1).W))) else None
-
+  val size = if (params.adjustableSize) Some(Input(UInt(log2Up(params.pingPongSize + 1).W))) else None 
+  
   val lastIn = Input(Bool())
   val lastOut = Output(Bool())
 }
@@ -56,56 +53,52 @@ class BitReversePingPong[T <: Data: Real](val params: BitReversePingPongParams[T
   val size = params.pingPongSize
   val memPing = SyncReadMem(size, params.proto)
   val memPong = SyncReadMem(size, params.proto)
-  val totalDataWidth = params.proto.real.getWidth * 2
+  val totalDataWidth = params.proto.real.getWidth*2
   memPing.suggestName("SRAM" + "_depth_" + size.toString + "_width_" + totalDataWidth.toString + s"_mem")
-
-  val memPongReal = SyncReadMem(size, params.proto.real)
-  val memPongImag = SyncReadMem(size, params.proto.imag)
 
   object StateFSM extends ChiselEnum {
     val sIdle, sWriteOnly, sReadWrite, sReadOnly = Value
   }
   val state = RegInit(StateFSM.sIdle)
-  val state_next = WireInit(StateFSM.sIdle)
+  val state_next = WireInit(StateFSM.sIdle) 
   val rstCntWritten = Wire(Bool())
   val rstCntRead = Wire(Bool())
   // Counter to keep track of how much data is written in memory
-  //val cntWritten = CounterWithReset(io.in.fire, size, rstCntWritten)
-  val readyWrite = state =/= StateFSM.sReadOnly //state_next =/= StateFSM.sReadOnly
+  //val cntWritten = CounterWithReset(io.in.fire(), size, rstCntWritten)
+  val readyWrite = state =/= StateFSM.sReadOnly//state_next =/= StateFSM.sReadOnly
   val cntWritten = CounterWithReset(io.in.valid && readyWrite, size, rstCntWritten)
-  //val cntRead = CounterWithReset(io.out.fire, size, rstCntRead)
+  //val cntRead = CounterWithReset(io.out.fire(), size, rstCntRead)
   val validRead = state === StateFSM.sReadOnly || state === StateFSM.sReadWrite
   val cntRead = CounterWithReset(io.out.ready && validRead, size, rstCntRead)
-
+  
   val cntWriteMax = cntWritten._1 === (io.size.getOrElse(size.U) - 1.U)
   val cntReadMax = cntRead._1 === (io.size.getOrElse(size.U) - 1.U)
-
+  
   dontTouch(cntWritten._1)
   cntWritten._1.suggestName("writeCounter")
-
+  
   rstCntWritten := cntWriteMax && (io.in.valid && readyWrite) || state_next === StateFSM.sIdle
   rstCntRead := cntReadMax && (io.out.ready && validRead) || state_next === StateFSM.sIdle
-
+  
   val readPing = RegInit(true.B) // read ping or pong
   val writePing = RegInit(true.B)
   val last = RegInit(false.B)
   val doubleRead = RegInit(false.B)
-
-  /*
+ 
+ /*
   val readAddress =   if (params.bitReverseDir) MuxCase(0.U(log2Size.W), cases) else cntRead._1
   val writeAddress = if (params.bitReverseDir) cntWritten._1 else MuxCase(0.U(log2Size.W), cases)
-   */
+  */
   val readAddress = Wire(cntRead._1.cloneType)
   val writeAddress = Wire(cntWritten._1.cloneType)
-
-  val log2Size = log2Up(size) // nummber of stages in the FFT case
+  
+  val log2Size = log2Up(size)                              // nummber of stages in the FFT case
   val subSizes = (1 to log2Size).map(d => pow(2, d).toInt) // all possible number of stages
   val subSizesWire = subSizes.map(e => e.U)
   val bools = subSizesWire.map(e => e === io.size.getOrElse(size.U)) // create conditions
-  val cases = bools.zip(1 to log2Size).map {
-    case (bool, numBits) =>
-      //bool -> { if (params.bitReverseDir) Reverse(cntRead._1(numBits-1, 0)) else Reverse(cntWritten._1(numBits-1, 0)) }
-      bool -> Reverse(cntWritten._1(numBits - 1, 0))
+  val cases = bools.zip(1 to log2Size).map { case (bool, numBits) =>
+    //bool -> { if (params.bitReverseDir) Reverse(cntRead._1(numBits-1, 0)) else Reverse(cntWritten._1(numBits-1, 0)) }
+    bool -> Reverse(cntWritten._1(numBits-1, 0))
   }
 
   readAddress := cntRead._1 //Mux(io.out.ready, cntRead._1, RegEnable(cntRead._1 , false.B, io.out.ready))
@@ -114,179 +107,135 @@ class BitReversePingPong[T <: Data: Real](val params: BitReversePingPongParams[T
   writeAddress := MuxCase(0.U(log2Size.W), cases)
 
   /*if (params.bitReverseDir)
-    readAddress := MuxCase(0.U(log2Size.W), cases)
-  else
+    readAddress := MuxCase(0.U(log2Size.W), cases) 
+  else 
     readAddress := cntRead._1
-
-  if (params.bitReverseDir)
+  
+  if (params.bitReverseDir) 
     writeAddress := cntWritten._1
-  else
+  else 
     writeAddress := MuxCase(0.U(log2Size.W), cases)*/
-
-  when(io.in.fire && io.lastIn) {
+  
+  when (io.in.fire() && io.lastIn) {
     last := true.B
   }
-  val lastRead = cntReadMax && io.out.ready
+  val lastRead = cntReadMax  && io.out.ready
   val lastWrite = cntWriteMax && io.in.valid
   val readOnly = lastWrite && ~lastRead
   val writeOnly = ~lastWrite && lastRead
   state_next := state // to be defined always
-
-  when(state === StateFSM.sIdle) {
+  
+  when (state === StateFSM.sIdle) {
     readPing := true.B
     writePing := true.B
-  }.otherwise {
-    when(lastRead) {
+  }
+  .otherwise {
+    when (lastRead) {
       readPing := ~readPing
     }
-    when(lastWrite) {
+    when (lastWrite) {
       writePing := ~writePing
     }
   }
-
-  val rstProtoIQ = Wire(io.in.bits.cloneType)
-  rstProtoIQ.real := Real[T].fromDouble(0.0)
-  rstProtoIQ.imag := Real[T].fromDouble(0.0)
-  val memPongData = WireDefault(rstProtoIQ)
-  val memPingData = WireDefault(rstProtoIQ)
-
-  if (params.singlePortSRAM) {
-    val addressPong = Mux(io.in.fire && (writePing === false.B), writeAddress, readAddress)
-    val addressPing = Mux(io.in.fire && (writePing === true.B), writeAddress, readAddress)
-
-    val rdWrPortPong = memPong(addressPong)
-    when(io.in.fire && ~writePing) {
-      rdWrPortPong := io.in.bits
-    }
-    memPongData := rdWrPortPong
-
-    val rdWrPortPing = memPing(addressPing)
-    when(io.in.fire && writePing) {
-      rdWrPortPing := io.in.bits
-    }
-    memPingData := rdWrPortPing
-  } else {
-    when(io.in.fire && writePing) {
-      //when(io.in.ready && readyWrite && writePing) {
-      memPing(writeAddress) := io.in.bits
-    }
-    when(io.in.fire && ~writePing) {
-      // when (io.in.ready && readyWrite && ~writePing) {
-      memPong(writeAddress) := io.in.bits
-    }
-    memPingData := memPing(readAddress)
-    memPongData := memPong(readAddress)
+  when (io.in.fire() && writePing) {
+  //when(io.in.ready && readyWrite && writePing) { 
+    memPing(writeAddress) := io.in.bits
   }
-
-  when(state === StateFSM.sIdle) {
-    when(io.in.fire) {
+  when (io.in.fire() && ~writePing) {
+ // when (io.in.ready && readyWrite && ~writePing) {
+   memPong(writeAddress) := io.in.bits
+  }
+  
+  when (state === StateFSM.sIdle) {
+    when (io.in.fire()) {
       state_next := StateFSM.sWriteOnly
     }
   }
-    .elsewhen(state === StateFSM.sWriteOnly) {
-      when(lastWrite) {
-        when(io.lastIn) { // this one should be checked
-          state_next := StateFSM.sReadOnly
-        }.otherwise {
-          /*.elsewhen (io.out.ready) {
+  .elsewhen (state === StateFSM.sWriteOnly) {
+    when (lastWrite) {
+      when (io.lastIn) { // this one should be checked
+        state_next := StateFSM.sReadOnly
+      }
+      .otherwise {
+      /*.elsewhen (io.out.ready) {
         state_next := StateFSM.sReadWrite
       }
       .otherwise {*/
-          //state_next := StateFSM.sWriteOnly
+        //state_next := StateFSM.sWriteOnly
+        state_next := StateFSM.sReadWrite
+      }
+      //}
+    }
+  }
+  .elsewhen (state === StateFSM.sReadWrite) {
+    when (readOnly) {
+      when (io.lastIn) {
+        doubleRead := true.B
+      }
+      state_next := StateFSM.sReadOnly
+    }
+    when (writeOnly) {
+      state_next := StateFSM.sWriteOnly
+    }
+    when (~writeOnly && ~readOnly && io.lastIn) {
+      state_next := StateFSM.sReadOnly
+    } 
+  }
+  .elsewhen (state === StateFSM.sReadOnly) {
+    when (lastRead) {
+      when (~doubleRead) { // not tested
+        when (last) {
+          state_next := StateFSM.sIdle
+          last := false.B
+        }
+        .otherwise {
           state_next := StateFSM.sReadWrite
         }
-        //}
+      }
+      .otherwise {
+        doubleRead := false.B
       }
     }
-    .elsewhen(state === StateFSM.sReadWrite) {
-      when(readOnly) {
-        when(io.lastIn) {
-          doubleRead := true.B
-        }
-        state_next := StateFSM.sReadOnly
-      }
-      when(writeOnly) {
-        state_next := StateFSM.sWriteOnly
-      }
-      when(~writeOnly && ~readOnly && io.lastIn) {
-        state_next := StateFSM.sReadOnly
-      }
-    }
-    .elsewhen(state === StateFSM.sReadOnly) {
-      when(lastRead) {
-        when(~doubleRead) { // not tested
-          when(last) {
-            state_next := StateFSM.sIdle
-            last := false.B
-          }.otherwise {
-            state_next := StateFSM.sReadWrite
-          }
-        }.otherwise {
-          doubleRead := false.B
-        }
-      }
-    }
-
+  }
+  
   state := state_next
   // naming ping/pong!
+  val memPingData = memPing(readAddress)
+  val memPongData = memPong(readAddress)
 
 //   io.out.bits  := Mux(RegNext(readPing), memPingData, memPongData)
 //   io.in.ready  := state =/= StateFSM.sReadOnly
 //   io.out.valid := RegNext(state === StateFSM.sReadOnly || state === StateFSM.sReadWrite)
 //   io.lastOut   := RegNext(last && state_next === StateFSM.sIdle)
-
-  val outQueue =
-    Module(
-      new Queue(chiselTypeOf(io.out.bits), entries = 1, pipe = false, flow = true)
-    ) // prev flow on true and pipe on true
-  outQueue.io.enq.bits := Mux(
-    RegNext(readPing),
-    memPingData,
-    memPongData
-  ) //Mux(RegNext(readPing), memPingData, memPongData)
-  outQueue.io.enq.valid := RegNext(
-    (state === StateFSM.sReadOnly || state === StateFSM.sReadWrite) && io.out.ready,
-    false.B
-  )
+  
+  val outQueue = Module(new Queue(chiselTypeOf(io.out.bits), entries = 1, pipe = false, flow = true)) // prev flow on true and pipe on true
+  outQueue.io.enq.bits := Mux(RegNext(readPing), memPingData, memPongData)//Mux(RegNext(readPing), memPingData, memPongData)
+  outQueue.io.enq.valid := RegNext((state === StateFSM.sReadOnly || state === StateFSM.sReadWrite) && io.out.ready, false.B)
   outQueue.io.deq.ready := io.out.ready
-
-  io.out.bits := outQueue.io.deq.bits
-  io.in.ready := state =/= StateFSM.sReadOnly
+  
+  io.out.bits  := outQueue.io.deq.bits
+  io.in.ready  := state =/= StateFSM.sReadOnly
   io.out.valid := outQueue.io.deq.valid
-
+  
   val outQueueLast = Module(new Queue(chiselTypeOf(io.lastIn), entries = 1, pipe = true, flow = true))
   outQueueLast.io.enq.bits := RegNext(last && state_next === StateFSM.sIdle, false.B)
-  outQueueLast.io.enq.valid := RegNext(
-    state === StateFSM.sReadOnly || state === StateFSM.sReadWrite && io.out.ready,
-    false.B
-  )
+  outQueueLast.io.enq.valid := RegNext(state === StateFSM.sReadOnly || state === StateFSM.sReadWrite && io.out.ready, false.B)
   outQueueLast.io.deq.ready := io.out.ready
-
+  
   // not logical but it works!
-  io.lastOut := state === StateFSM.sIdle && io.out.fire //outQueueLast.io.deq.bits
+  io.lastOut   := state === StateFSM.sIdle && io.out.fire() //outQueueLast.io.deq.bits
   dontTouch(io.lastOut)
 }
 
-object BitReversePingPongApp extends App {
+/*object BitReversePingPongApp extends App
+{
 
   val params = BitReversePingPongParams.fixed(
     dataWidth = 24,
     binPoint = 9,
     pingPongSize = 1024,
     bitReverseDir = true,
-    adjustableSize = true
+    adjustableSize = true,
   )
-//   (new ChiselStage).execute(
-//     Array("--target-dir", "verilog/BitReversePingPong"),
-//     Seq(ChiselGeneratorAnnotation(() => new BitReversePingPong(params)))
-//   )
-  val arguments = Array(
-    "-X",
-    "verilog",
-    "--repl-seq-mem",
-    "-c:BitReversePingPong:-o:mem.conf",
-    "--log-level",
-    "info"
-  )
-  (new ChiselStage).execute(arguments, Seq(ChiselGeneratorAnnotation(() => new BitReversePingPong(params))))
-}
+  chisel3.Driver.execute(args, () => new BitReversePingPong(params))
+}*/
--- src/test/scala/BitReversePingPongSpec.scala
+++ src/test/scala/BitReversePingPongSpec.scala
@@ -5,8 +5,7 @@ package fft
 import chisel3.experimental.FixedPoint
 import chisel3.iotesters.Driver
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+import org.scalatest.{FlatSpec, Matchers}
 import chisel3.util.log2Up
 
 import dsptools._
@@ -16,57 +15,58 @@ import breeze.math.Complex
 import breeze.signal.fourierTr
 import breeze.linalg.DenseVector
 
-import scala.math.{pow, Pi}
+import scala.math.{Pi, pow}
 import scala.util.Random
 
 class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) extends DspTester(c) {
-
+ 
   // move this to trait and then use extends
   /**
-    * Returns bit reversed index
-    */
+  * Returns bit reversed index
+  */
   def bit_reverse(in: Int, width: Int): Int = {
     import scala.math.pow
     var test = in
     var out = 0
     for (i <- 0 until width) {
-      if (test / pow(2, width - i - 1) >= 1) {
-        out += pow(2, i).toInt
-        test -= pow(2, width - i - 1).toInt
+      if (test / pow(2, width-i-1) >= 1) {
+        out += pow(2,i).toInt
+        test -= pow(2,width-i-1).toInt
       }
     }
     out
   }
 
   /**
-    * Reordering data
-    */
+   * Reordering data
+   */
   def bitrevorder_data(testSignal: Seq[Complex]): Seq[Complex] = {
     val seqLength = testSignal.size
     val new_indices = (0 until seqLength).map(x => bit_reverse(x, log2Up(seqLength)))
     new_indices.map(x => testSignal(x))
   }
-
+  
   def testReadyOut(inp: Seq[Complex], tol: Int = 3) {
-
+    
     val input = if (c.params.bitReverseDir) bitrevorder_data(inp) else inp
     val output = bitrevorder_data(input)
     val pingPongSize = c.params.pingPongSize
-
+    
     println("Expected result should be: ")
     output.map(c => println((c).toString))
-
+    
     var cntValidIn = 0
     var cntValidOut = 0
     poke(c.io.out.ready, 1)
     poke(c.io.in.valid, 1)
-
+    
     // flush after only one window
     while (cntValidIn < input.length) {
       if (cntValidIn == (input.length - 1)) {
         poke(c.io.lastIn, 1)
         poke(c.io.in.bits, input(cntValidIn))
-      } else {
+      }
+      else {
         poke(c.io.in.bits, input(cntValidIn))
       }
       cntValidIn += 1
@@ -74,13 +74,14 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
     }
     poke(c.io.in.valid, 0)
     poke(c.io.lastIn, 0)
-
+    
     while (cntValidOut < output.length) {
       if (cntValidOut == (output.length - 1) && peek(c.io.out.valid)) {
         expect(c.io.lastOut, 1)
         fixTolLSBs.withValue(tol) { expect(c.io.out.bits, output(cntValidOut)) }
         cntValidOut += 1
-      } else {
+      }
+      else {
         if (peek(c.io.out.valid)) {
           fixTolLSBs.withValue(tol) { expect(c.io.out.bits, output(cntValidOut)) }
           cntValidOut += 1
@@ -93,7 +94,7 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
 
     step(3)
     poke(c.io.in.valid, 1)
-
+    
     while (cntValidIn < input.length) {
       poke(c.io.in.bits, input(cntValidIn))
       cntValidIn += 1
@@ -149,7 +150,7 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
     cntValidIn = 0
     step(10)
   }
-
+  
   def testStream(inp: Seq[Complex], tol: Int = 3) {
 
     val input = if (c.params.bitReverseDir) bitrevorder_data(inp) else inp
@@ -169,7 +170,8 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
       if (cntValidIn == (input.length - 1)) {
         poke(c.io.lastIn, 1)
         poke(c.io.in.bits, input(cntValidIn))
-      } else {
+      }
+      else {
         poke(c.io.in.bits, input(cntValidIn))
       }
       cntValidIn += 1
@@ -183,7 +185,8 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
         expect(c.io.lastOut, 1)
         fixTolLSBs.withValue(tol) { expect(c.io.out.bits, output(cntValidOut)) }
         cntValidOut += 1
-      } else {
+      }
+      else {
         if (peek(c.io.out.valid)) {
           fixTolLSBs.withValue(tol) { expect(c.io.out.bits, output(cntValidOut)) }
           cntValidOut += 1
@@ -207,7 +210,8 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
       if (cntValidIn == (input.length - 1)) {
         poke(c.io.lastIn, 1)
         poke(c.io.in.bits, input(cntValidIn))
-      } else {
+      }
+      else {
         poke(c.io.in.bits, input(cntValidIn))
       }
       cntValidIn += 1
@@ -218,8 +222,8 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
       step(1)
     }
     // check flushing when both memories are full
-    // it works looking vcd diagram but tester gives an error
-    /* cntValidOut = 0
+   // it works looking vcd diagram but tester gives an error
+   /* cntValidOut = 0
     while (cntValidOut < output.length) {
       if (cntValidOut == (output.length - 1) && peek(c.io.out.valid)) {
         expect(c.io.lastOut, 1)
@@ -239,36 +243,37 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
   }
 
   def testAdjustableSize(inp: Seq[Complex], tol: Int = 3) {
-    require(c.params.adjustableSize == true, "Interface must have included size input")
+    require (c.params.adjustableSize == true, "Interface must have included size input")
 
     val log2Size = log2Up(c.params.pingPongSize)
-    var sizeColl: Seq[Int] = Seq.empty
-    sizeColl = (1 to log2Size).map(stage => pow(2, stage).toInt)
-
+    var sizeColl : Seq[Int] = Seq.empty
+    sizeColl = (1 to log2Size).map(stage => pow(2,stage).toInt)
+     
     var idx = 0
     var cntValid = 0
     poke(c.io.out.ready, 1)
     println(sizeColl.length.toString)
-
+    
     while (idx < 1) {
       val sizeCollShuffle = Random.shuffle(sizeColl)
       for (size <- sizeCollShuffle) {
         println("Current fft size is: ")
         println(size.toString)
-
+        
         val input = if (c.params.bitReverseDir) bitrevorder_data(inp.take(size)) else inp.take(size)
         val output = bitrevorder_data(input)
-
+        
+      
         val input1 = input.iterator
         val input2 = input.iterator
         val input3 = input.iterator
-
+        
         cntValid = 0
         println("Expected result should be: ")
         output.map(c => println(c.toString))
-
+    
         poke(c.io.in.valid, 0)
-        poke(c.io.size.get, size) //
+        poke(c.io.size.get, size)//
         step(5)
         poke(c.io.in.valid, 1)
 
@@ -283,9 +288,9 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
               case dspR: DspReal => realTolDecPts.withValue(tol) { expect(c.io.out.bits, output(cntValid)) }
               case _ => fixTolLSBs.withValue(tol) { expect(c.io.out.bits, output(cntValid)) }
             }
-            if (cntValid == (size - 1))
+            if (cntValid == (size-1))
               cntValid = 0
-            else
+            else 
               cntValid += 1
           }
           step(1)
@@ -297,38 +302,37 @@ class BitReversePingPongTester[T <: chisel3.Data](c: BitReversePingPong[T]) exte
               case dspR: DspReal => realTolDecPts.withValue(tol) { expect(c.io.out.bits, output(cntValid)) }
               case _ => fixTolLSBs.withValue(tol) { expect(c.io.out.bits, output(cntValid)) }
             }
-            if (cntValid == (size - 1))
+            if (cntValid == (size-1))
               cntValid = 0
-            else
+            else 
               cntValid += 1
           }
           step(1)
         }
         poke(c.io.in.valid, 0)
-        step(2 * inp.length)
+        step(2*inp.length)
         reset(2)
       }
-      idx += 1
+        idx += 1
     }
   }
 }
 
 object FixedPingPongTester {
   def apply(params: BitReversePingPongParams[FixedPoint], inp: Seq[Complex]): Boolean = {
-    chisel3.iotesters.Driver.execute(Array("-tbn", "verilator"), () => new BitReversePingPong(params)) { c =>
-      new BitReversePingPongTester(c) {
-        if (params.adjustableSize) testAdjustableSize(inp) else testStream(inp) //testReadyOut(inp)
-      }
-    }
+		chisel3.iotesters.Driver.execute(Array("-tbn", "verilator"),
+      () => new BitReversePingPong(params)) { c => new BitReversePingPongTester(c) {
+        if (params.adjustableSize) testAdjustableSize(inp) else testStream(inp)//testReadyOut(inp)
+      }}
   }
 }
-class BitReversePingPongSpec extends AnyFlatSpec with Matchers {
+class BitReversePingPongSpec extends FlatSpec with Matchers {
 
   // Add this to get same test case every time
   Random.setSeed(11110L)
-  val pingPongSize = 16
+  val pingPongSize  = 8
   val testSignal = (0 until pingPongSize).map(x => Complex(Random.nextDouble(), Random.nextDouble()))
-
+  
   it should f"test bitreverse ping-pong without adjustableSize and bitReverseDir = true" in { // DIF
     // input is in bitreverse order
     // output is in natural
@@ -338,10 +342,10 @@ class BitReversePingPongSpec extends AnyFlatSpec with Matchers {
       pingPongSize = pingPongSize,
       adjustableSize = false
     )
-    FixedPingPongTester(paramsPingPong, testSignal) should be(true)
+    FixedPingPongTester(paramsPingPong, testSignal) should be (true)
   }
-
-  it should f"test bitreverse ping-pong without adjustableSize and bitReverseDir = false" in { // DIT
+  
+  it should f"test bitreverse ping-pong without adjustableSize and bitReverseDir = false" in {  // DIT
     // input is in natural order
     // output is in bit reverse order
     val paramsPingPong = BitReversePingPongParams.fixed(
@@ -351,9 +355,9 @@ class BitReversePingPongSpec extends AnyFlatSpec with Matchers {
       adjustableSize = false,
       bitReverseDir = false
     )
-    FixedPingPongTester(paramsPingPong, testSignal) should be(true)
+    FixedPingPongTester(paramsPingPong, testSignal) should be (true)
   }
-
+  
   it should f"test bitreverse ping-pong with adjustableSize and bitReverseDir = true" in {
     // input is in bitreverse order
     // output is in natural
@@ -364,21 +368,7 @@ class BitReversePingPongSpec extends AnyFlatSpec with Matchers {
       adjustableSize = true,
       bitReverseDir = true
     )
-    FixedPingPongTester(paramsPingPong, testSignal) should be(true)
-  }
-
-  it should f"test bitreverse ping-pong with adjustableSize and bitReverseDir = true and single port SRAMs" in {
-    // input is in bitreverse order
-    // output is in natural
-    val paramsPingPong = BitReversePingPongParams.fixed(
-      dataWidth = 16,
-      binPoint = 14,
-      pingPongSize = pingPongSize,
-      adjustableSize = true,
-      bitReverseDir = true,
-      singlePortSRAM = true
-    )
-    FixedPingPongTester(paramsPingPong, testSignal) should be(true)
+    FixedPingPongTester(paramsPingPong, testSignal) should be (true)
   }
 
   // Note: for testing this test case, ignore should be in and testReadyOut function should be called
@@ -391,6 +381,7 @@ class BitReversePingPongSpec extends AnyFlatSpec with Matchers {
       adjustableSize = false,
       bitReverseDir = false
     )
-    FixedPingPongTester(paramsPingPong, testSignal) should be(true)
+    FixedPingPongTester(paramsPingPong, testSignal) should be (true)
   }
 }
+
